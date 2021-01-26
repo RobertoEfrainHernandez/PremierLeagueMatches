d8ac35009047ce7fce2c142515126ca6dc1ade52
Roberto Efrain Hernandez <bh21346n@pace.edu>
2021-01-26 10:37:06 -0800

    Initial Commit

---
+++ PremierLeagueMatches/PremierLeagueMatchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremierLeagueMatchesApp.swift
+//  PremierLeagueMatches
+//
+//  Created by Roberto Hernandez on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PremierLeagueMatchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
